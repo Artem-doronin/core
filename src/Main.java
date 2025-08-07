f96d1e193e0b61433319bf5decfa028d9e8f908c
Artem Doronin <doronintema2018@yandex.ru>
2025-08-07 15:04:09 +0500

    clear

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,4 @@
-public class Main {
+public  class Main {
     public static void main(String[] args) {
-        MyStringBuilder myStringBuilder = new MyStringBuilder();
-        myStringBuilder.append("Hello");
-        System.out.println(myStringBuilder);
-        myStringBuilder.append(" World");
-        System.out.println(myStringBuilder);
-        myStringBuilder.undo();
-        System.out.println(myStringBuilder);
     }
 }
